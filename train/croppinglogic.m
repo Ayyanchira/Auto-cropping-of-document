baa4f6ce70c4ede17737e8db1ef6aea3845f875f
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-25 11:39:01 -0500

    Removing the commented code
    Removing commented code which was used to visualize images after taking their r,g,b values. That was the approach I used for auto crop previous challenge. That wont work this time because the images are rotated and finding the right edges wont work that way

--- train/croppinglogic.m
+++ train/croppinglogic.m
@@ -12,19 +12,7 @@ for i = 1:25
     %     [R,C] = size(edgeG);
     [R,C] = size(edgeG);
 d = ceil(sqrt(R^2 + C^2)); 
-    %     im1 = edgeG(1:floor(R/2),1:C);
-    %     im2 = edgeG(1:R,1:floor(C/2));
-    %     im3 = edgeG(floor(R/2):R,1:C);
-    %     im4 = edgeG(1:R,floor(C/2):C);
-    %
-    %     subplot(2,2,1);
-    %     imshow(im1);
-    %     subplot(2,2,2);
-    %     imshow(im2);
-    %     subplot(2,2,3);
-    %     imshow(im3);
-    %     subplot(2,2,4);
-    %     imshow(im4);
+
     %% Generating accumulator array
     accum = zeros(2*d+1,180);
     
@@ -39,30 +27,7 @@ d = ceil(sqrt(R^2 + C^2));
     figure('name','Accumulator Array');
     title('Accumulator Array without non-max suppression');
     imshow(accum(1:10:end,:),[]); colormap jet;
-    %     level = graythresh(f);
-    %     rgb1 = f(:,:,1);
-    % %     figure;
-    % %     imshow(rgb1);
-    %     rgb2 = f(:,:,2);
-    % %     figure;
-    % %     imshow(rgb2);
-    %     rgb3 = f(:,:,3);
-    % %     figure;
-    % %     imshow(rgb3);
-    %
-    %     g1 = imbinarize(rgb1,level);
-    % %     figure;
-    % %     imshow(g1);
-    %     g2 = imbinarize(rgb2,level);
-    % %     figure;
-    % %     imshow(g2);
-    %     g3 = imbinarize(rgb3,level);
-    % %     figure;
-    % %     imshow(g3);
-    %
-    %     sumI = g1&g2&g3;
-    % %     figure;
-    % %     imshow(sumI);
+
     close all;
     
 end
