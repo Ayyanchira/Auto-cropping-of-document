ba0bac83ce90f9563e27f8268b588cba9868ffe9
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-25 17:09:41 -0500

    Trying histogram equalization to bring everything in range

--- train/FilteringOptions.m
+++ train/FilteringOptions.m
@@ -4,10 +4,13 @@ for i = 1:25
     folder_name = 'data/';
     fn = sprintf ( '%sinput_%02d.jpg%', folder_name, i);
     f = imread ( fn );
-    g = imadjust(rgb2gray(f));
     
+    h = histeq(f);
+    g = imadjust(rgb2gray(h));
+%     figure;
+%     imshow(h);
     edgeG = edge(g);
-    BW2 = bwareaopen(edgeG,20);
+    BW2 = bwareaopen(edgeG,10);
     figure;
     imshow(BW2);
 end
