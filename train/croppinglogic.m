841c8c0c31deec1f500a08da10873c8dbdeb6b50
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-25 12:14:36 -0500

    Trying hough2 transform without non max suppression
    Found that it is detecting edges which are in foreground as well! Highly probable that it will disturb the cropping algorithm

--- train/croppinglogic.m
+++ train/croppinglogic.m
@@ -11,7 +11,7 @@ for i = 1:25
     
     %     [R,C] = size(edgeG);
     [R,C] = size(edgeG);
-d = ceil(sqrt(R^2 + C^2)); 
+    d = ceil(sqrt(R^2 + C^2)); 
 
     %% Generating accumulator array
     accum = zeros(2*d+1,180);
@@ -27,7 +27,33 @@ d = ceil(sqrt(R^2 + C^2));
     figure('name','Accumulator Array');
     title('Accumulator Array without non-max suppression');
     imshow(accum(1:10:end,:),[]); colormap jet;
-
     close all;
-    
+    %% Edges without non max suppression
+    sortedMaxValues = sort(accum(:),'descend');
+    top4 = sortedMaxValues(1:4);
+    figure('name','Accumulator Array');
+    title('Accumulator Array without non-max suppression');
+    imshow(accum(1:10:end,:),[]); colormap jet;
+    colorIm = f;
+    for line = 1:4
+        lineValue = top4(line);
+        [I,J] = find(accum == lineValue);
+        k = I(1);
+        %get actual rho
+        rho = k-d-1;
+        theta = J(1)-1;
+        %     y = mx+c
+        x1 = [];
+        y1 = [];
+        for x = 1:C
+            %         y = round(-x*cotd(theta)) + round(rho*cscd(theta));
+            y = (rho - x*sind(theta))/cosd(theta);
+            x1 = [x1,x];
+            y1 = [y1,y];
+        end
+        colorIm = insertShape(colorIm,'line',[x1(1),y1(1),x1(end),y1(end)]);
+    end
+    figure('name','Lines without non-max suppression');
+    title('Top 4 lines without non-max suppression');
+    imshow(colorIm);
 end
